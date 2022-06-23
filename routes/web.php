2c09dee859b36e2dae3ed82999522b8e52615beb
unknown <henriquealbino1@gmail.com>
2022-06-22 21:28:13 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::put('admin/plans/{url}','Admin\PlanController@update')->name('plan.update');
-Route::get('admin/plans/{url}/edit','Admin\PlanController@edit')->name('plan.edit');
-Route::any('admin/plans/search', 'Admin\PlanController@search')->name('plan.search');
-Route::get('admin/plans/create','Admin\PlanController@create')->name('plan.create');
-Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plan.destroy');
-Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plan.show');
-Route::post('admin/plans/store', 'Admin\PlanController@store')->name('plan.store');
-Route::get('admin/plans','Admin\PlanController@index')->name('plan.index');
+Route::prefix('admin')->namespace('Admin')->group(function(){
+    Route::put('plans/{url}','PlanController@update')->name('plan.update');
+    Route::get('plans/{url}/edit','PlanController@edit')->name('plan.edit');
+    Route::any('plans/search', 'PlanController@search')->name('plan.search');
+    Route::get('plans/create','PlanController@create')->name('plan.create');
+    Route::delete('plans/{url}', 'PlanController@destroy')->name('plan.destroy');
+    Route::get('plans/{url}', 'PlanController@show')->name('plan.show');
+    Route::post('plans/store', 'PlanController@store')->name('plan.store');
+    Route::get('plans','PlanController@index')->name('plan.index');
 
-Route::get('admin','Admin\PlanController@index')->name('admin.index');
+    Route::get('/','PlanController@index')->name('admin.index');
+
+});
+
+
 
 Route::get('/', function () {
     return view('welcome');
